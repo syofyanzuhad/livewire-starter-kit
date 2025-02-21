3484bfdb6207fad2e37658978ba8db7d9a11f628
Taylor Otwell <taylor@laravel.com>
2025-02-21 10:57:19 -0600

    work on settings pages

--- routes/settings.php
+++ routes/settings.php
@@ -4,24 +4,12 @@ use Illuminate\Support\Facades\Route;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware([
     'auth',
     ValidateSessionWithWorkOS::class,
 ])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
-
-Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
